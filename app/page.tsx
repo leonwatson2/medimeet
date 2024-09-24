aaa712b3478c4e95e835b5e370545c8dc9308466
Leon Watson <leon@vlw2.com>
2024-09-24 13:25:18 -0500

    setup with shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "../components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-    <h1>Home</h1>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+      </section>
     </div>
   );
 }
